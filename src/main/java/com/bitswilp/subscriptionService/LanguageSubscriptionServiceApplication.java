5122bb19d745c04b937503ce66ca062dcf77caf0
Sharanya <learningautomation27@gmail.com>
2024-11-20 21:48:30 +0530

    Integrated language subscription with learn service

--- src/main/java/com/bitswilp/subscriptionService/LanguageSubscriptionServiceApplication.java
+++ src/main/java/com/bitswilp/subscriptionService/LanguageSubscriptionServiceApplication.java
@@ -2,8 +2,20 @@ package com.bitswilp.subscriptionService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Language Subscription Microservice Swagger",
+	        version = "v1",
+	        description = "API documentation for My Microservice"
+	    )
+	)
 public class LanguageSubscriptionServiceApplication {
 
 	public static void main(String[] args) {
